7911eac06aa20628bb409099f0f4def8f7367d53
Divya <divya.bettampadi@gmail.com>
2022-09-04 17:40:18 +0530

    Function to check if a number is prime or not

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 function isPrime(num){
+	var prime = 1;
+	if(num == 0 || num == 1){
+		console.log("0 and 1 are neither prime or composite");
+	}else if(num == 2){
+		console.log(num+" is a prime number");
+	}else{
+		for(var i=2; i<num/2; i++){
+			if(num %i == 0){
+				prime = 0;
+				break;
+			}
+		}
+	}
+	if(prime == 1){
+		console.log("Number is prime");
+	}else{
+		console.log("Number is composite");
+	}
 }
+
+isPrime(2);
+isPrime(10);
+isPrime(31);
